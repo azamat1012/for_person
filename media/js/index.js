7072999c936aa758666ddcce3499a10cbf1f96a3
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-02-17 20:07:29 +0500

    Add files via upload

---
+++ media/js/index.js
@@ -0,0 +1,17 @@
+function second_function(prop){
+    if(prop==="yes"){
+        return window.location.href = "./yes.html";
+    }else{
+        return window.location.href = "./no.html";
+    }
+}   
+
+
+function third_function(){
+    return window.location.href = "./third.html";
+}
+
+function fourth_function(){
+    return window.location.href = "./fourth.html";
+
+}
